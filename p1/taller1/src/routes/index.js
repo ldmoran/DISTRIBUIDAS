86bfeb1f0932a03a03487ce128420369dd39ab90
MORAN PALOMO LENIN DAVID <ldmoran@espe.edu.ec>
2026-05-06 10:41:37 -0500

    Add route for register page in index.js

--- p1/taller1/src/routes/index.js
+++ p1/taller1/src/routes/index.js
@@ -6,4 +6,7 @@ const views = path.join(__dirname, '/../views');//ruta de la carpeta de vistas
 router.get('/', (req, res) => {
     res.sendFile(views + "/index.html");//enviar el archivo index.html como respuesta a la ruta raíz
 });
+router.get('/register', (req, res) => {
+    res.sendFile(views + "/register.html");//enviar el archivo chat.html como respuesta a la ruta /chat
+});
 module.exports = router;//exportar el router para usarlo en el archivo principal
